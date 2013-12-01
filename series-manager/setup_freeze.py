51e90b91fdbc4ec3bae0f6a51f7d5baa8610c4e3
Ronnie <ronnieandrew92@gmail.com>
2013-12-01 20:00:37 -0200

    imageformats setup_freeze.py

--- series-manager/setup_freeze.py
+++ series-manager/setup_freeze.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "excludes": ["tkinter"], "include_files": ["images/"]}
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"], "include_files": ["images/", "imageformats/"]}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
